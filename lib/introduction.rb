87fbd95c0b9584fa851a179d6114a13ab1fed77e
Erik Prenzlin <eprenzlin@gmail.com>
2019-12-28 12:19:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,14 +14,12 @@ def introduction_with_language (name,language)
   puts " Hi, my name is #{name}, and I am learning to program in #{language}"
 end
 
-introduction_with_language ( "Charles", "Python")
 
 def introduction_with_language_optional(name,language = "Ruby")
   puts " Hi, my name is #{name}, and I am learning to program in #{language}"
-  end
+end
 
-introduction_with_language_optional ( " Charles", language )
-  
+
 
 
 
